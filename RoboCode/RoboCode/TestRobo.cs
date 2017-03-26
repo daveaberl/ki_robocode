1cd87ec62ba177182c8170fb8552dd5d5fb4e59e
David Aberl <david.aberl@outlook.com>
2017-03-26 10:47:55 +0200

    updated yolo-names

--- RoboCode/RoboCode/TestRobo.cs
+++ RoboCode/RoboCode/TestRobo.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RobocodeTest
+namespace YoloSpace
 {
-    class TestRobo : Robot
+    class YoloBot : Robot
     {
         public override void Run()
         {
