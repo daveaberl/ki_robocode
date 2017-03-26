22cd012ed63bd8fb319f4f6bdd15f68518a29272
David Aberl <david.aberl@outlook.com>
2017-03-26 13:28:52 +0200

    added transition to KillItWithFire

---
+++ RoboCode/RoboCode/EnemyRobot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoloSpace
+{
+    class EnemyRobot
+    {
+        //
+        // Zusammenfassung:
+        //     Returns the bearing to the robot you scanned, relative to your robot's heading,
+        //     in degrees (-180 <= getBearing() < 180)
+        public double Bearing { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the bearing to the robot you scanned, relative to your robot's heading,
+        //     in radians (-PI <= getBearingRadians() < PI)
+        public double BearingRadians { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the distance to the robot (your center to his center).
+        public double Distance { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the energy of the robot.
+        public double Energy { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the heading of the robot, in degrees (0 <= getHeading() < 360)
+        public double Heading { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the heading of the robot, in radians (0 <= getHeading() < 2 * PI)
+        public double HeadingRadians { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the name of the robot.
+        public string Name { get; }
+        //
+        // Zusammenfassung:
+        //     Returns the velocity of the robot.
+        public double Velocity { get; }
+        //
+        // Zusammenfassung:
+        //     true if the scanned robot is a sentry robot; false otherwise.
+        public bool IsSentryRobot { get; }
+    }
+}
--- RoboCode/RoboCode/YoloBot.cs
+++ RoboCode/RoboCode/YoloBot.cs
@@ -12,20 +12,81 @@ namespace YoloSpace
         private const double DANGER_THRESHOLD = 0x0;
 
         private RoboPhase CurrentPhase { get; set; } = RoboPhase.ArenaObservation;
-        private Dictionary<Robot, double> robotDanger = new Dictionary<Robot, double>();
+        private Dictionary<ScannedRobotEvent, double> robotDanger =
+            new Dictionary<ScannedRobotEvent, double>();
+
+        private Dictionary<string, ScannedRobotEvent> robots =
+            new Dictionary<string, ScannedRobotEvent>();
+
+        private HitByBulletEvent lastBulletHit;
 
         private void ArenaObservation()
         {
-            
+            CurrentPhase = RoboPhase.MeetAndGreet;
+        }
+
+        private void FollowWall()
+        {
+            // Check wall
+
+            SetAhead(10);
+        }
+
+        private double CalculateDanger(ScannedRobotEvent enemy)
+            => enemy.Distance;
+
+        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        {
+            base.OnScannedRobot(evnt);
+
+            robotDanger[evnt] = CalculateDanger(evnt);
+            robots[evnt.Name] = evnt;
         }
 
         private void MeetAndGreet()
         {
-            
+            FollowWall();
+            TurnRadarLeft(10);
+
+            Execute();
+        }
+
+        public override void OnHitByBullet(HitByBulletEvent evnt)
+        {
+            if (lastBulletHit == null &&
+                CurrentPhase == RoboPhase.MeetAndGreet)
+            {
+                lastBulletHit = evnt;
+                CurrentPhase = RoboPhase.KillItWithFire;
+            }
+
+            base.OnHitByBullet(evnt);
+        }
+        public override void OnRobotDeath(RobotDeathEvent evnt)
+        {
+            if (lastBulletHit?.Name == evnt.Name)
+            {
+                lastBulletHit = null;
+                CurrentPhase = RoboPhase.MeetAndGreet;
+            }
+
+            base.OnRobotDeath(evnt);
         }
 
         private void KillItWithFire()
         {
+            FollowWall();
+
+            if (lastBulletHit != null)
+            {
+                if (lastBulletHit.Bearing < 0)
+                    SetTurnRadarLeft(lastBulletHit.Bearing);
+                else
+                    SetTurnRadarRight(lastBulletHit.Bearing);
+            }
+
+
+            Execute();
         }
 
         public override void Run()
