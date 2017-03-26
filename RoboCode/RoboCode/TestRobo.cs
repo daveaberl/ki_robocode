df7173328e1715755be3392e3d95a7d60495f2d3
rene.kathofer <rene.kathofer@hotmail.com>
2017-03-26 13:19:36 +0200

    first phase

--- RoboCode/RoboCode/TestRobo.cs
+++ RoboCode/RoboCode/TestRobo.cs
@@ -7,14 +7,58 @@ using System.Threading.Tasks;
 
 namespace RobocodeTest
 {
-    class TestRobo : Robot
+    class TestRobo : AdvancedRobot
     {
+        private const double OFFSET = 20;
+
+        enum Direction { NORTH, EAST, SOUTH, WEST, UNKOWN };
+        private Direction currentDirection = Direction.UNKOWN;
+
         public override void Run()
         {
-            while (true)
+            DriveToWall();
+        }
+
+        private Direction DetermineDirection()
+        {
+            TurnLeft(Heading % 90);
+            if (Heading == 270)
+                return Direction.WEST;
+            else if (Heading == 90)
+                return Direction.EAST;
+            else if (Heading == 180)
+                return Direction.SOUTH;
+            else if (Heading == 0)
+                return Direction.NORTH;
+            return Direction.UNKOWN;
+        }
+
+        private double DetermineDistance(Direction direction)
+        {
+            switch (direction)
             {
-                // TODO: Kill it with fire
+                case Direction.NORTH:
+                    return BattleFieldHeight - Y;
+                case Direction.EAST:
+                    return BattleFieldWidth - X;
+                case Direction.SOUTH:
+                    return Y;
+                case Direction.WEST:
+                    return X;
+                default:
+                    return 0;
             }
+        }
+
+        private void DriveToWall()
+        {
+            currentDirection = DetermineDirection();
+            double distance = DetermineDistance(currentDirection);
+            Ahead(distance - OFFSET);
+            TurnLeft(1);
+            TurnLeft(Heading % 90);
+            TurnGunLeft(1);
+            TurnGunLeft(GunHeading % 90);
         }
     }
 }
