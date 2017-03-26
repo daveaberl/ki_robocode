c9c76e7b5f1757ef0eee65b27a652607a01bb7db
David Aberl <david.aberl@outlook.com>
2017-03-26 10:30:13 +0200

    added project

---
+++ RoboCode/RoboCode/TestRobo.cs
@@ -0,0 +1,20 @@
+﻿using Robocode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobocodeTest
+{
+    class TestRobo : Robot
+    {
+        public override void Run()
+        {
+            while (true)
+            {
+                // TODO: Kill it with fire
+            }
+        }
+    }
+}
