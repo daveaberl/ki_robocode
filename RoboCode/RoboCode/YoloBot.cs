ea815f2b0b3e15e778f31463b954d4e89f724fb7
David Aberl <david.aberl@outlook.com>
2017-03-26 12:01:50 +0200

    changed to AdvancedRobot

--- RoboCode/RoboCode/YoloBot.cs
+++ RoboCode/RoboCode/YoloBot.cs
@@ -7,19 +7,21 @@ using System.Threading.Tasks;
 
 namespace YoloSpace
 {
-    class YoloBot : Robot
+    class YoloBot : AdvancedRobot
     {
         private const double DANGER_THRESHOLD = 0x0;
 
-        private RoboPhase currentPhase = RoboPhase.ArenaObservation;
+        private RoboPhase CurrentPhase { get; set; } = RoboPhase.ArenaObservation;
         private Dictionary<Robot, double> robotDanger = new Dictionary<Robot, double>();
 
         private void ArenaObservation()
         {
+            
         }
 
         private void MeetAndGreet()
         {
+            
         }
 
         private void KillItWithFire()
@@ -30,7 +32,7 @@ namespace YoloSpace
         {
             while (true)
             {
-                switch (currentPhase)
+                switch (CurrentPhase)
                 {
                     case RoboPhase.ArenaObservation:
                         ArenaObservation();
